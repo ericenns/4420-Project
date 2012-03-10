f8db53c90429c021d5c1d8c12b191b20b9f89ca1
travismartindale <travis.j.martindale@gmail.com>
2012-03-10 14:02:06 -0600

    Added base tree class.

---
+++ src/project/dictionary/comparison/tree/Tree.java
@@ -0,0 +1,9 @@
+package project.dictionary.comparison.tree;
+
+public abstract class Tree {
+	
+	public abstract boolean search(int key);
+	public abstract boolean insert(int key);
+	public abstract boolean delete(int key);
+	
+}
