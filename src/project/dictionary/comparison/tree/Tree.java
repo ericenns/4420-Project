9b88414d2622e63d1c82393181e8f9cf8af259a5
travismartindale <travis.j.martindale@gmail.com>
2012-03-10 14:08:56 -0600

    Added abstract predecessor method.

--- src/project/dictionary/comparison/tree/Tree.java
+++ src/project/dictionary/comparison/tree/Tree.java
@@ -5,5 +5,5 @@ public abstract class Tree {
 	public abstract boolean search(int key);
 	public abstract boolean insert(int key);
 	public abstract boolean delete(int key);
-	
+	public abstract boolean predecessor(int key);
 }
